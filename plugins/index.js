5c75da8a86210f0db96d9f52df97c14216f9520e
taychin5lab <tay.161.tod@gmail.com>
2020-12-07 13:02:42 +0700

    add hello to head

--- plugins/index.js
+++ plugins/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
-import { Table, Input } from "buefy";
+import { Table, Input, Button } from "buefy";
 import "buefy/dist/buefy.css";
 
 Vue.use(Table);
 Vue.use(Input);
+Vue.use(Button);
