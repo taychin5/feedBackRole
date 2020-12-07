614df7dca8b5edb7ec818ac14fb72ad0f19bed27
tenpatrawin <w.patrawin@gmail.com>
2020-12-07 17:37:38 +0700

    Detail page layout

--- pages/role/_slug.vue
+++ pages/role/_slug.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="container">
     
-    <div><n-link to="/">back to the bundle</n-link></div>
-    <div class="grid grid-cols-2">
-      <div>
-        <img v-bind:src="role.mainImage" alt="" />
+    <div><n-link to="/"> back to the bundle </n-link></div>
+    <div class="grid grid-cols-5 mt-16">
+      <div class="col-span-2">
+        <img class="w-3/4 mx-auto" v-bind:src="role.mainImage" alt="" />
       </div>
-      <div>
-        <h1 class="text-5xl">{{role.title}}</h1>
-        <div>{{role.description}}</div>
-        <div class="p-8">
-          <h3 class="text-3xl">feedback guide</h3>
+      <div class="col-span-3 ml-20">
+        <h1 class="text-5xl font-bold">{{role.title}}</h1>
+        <div class="pb-8">{{role.description}}</div>
+        <div class="box-content border-none bg-white rounded-xl px-8 py-4 shadow">
+          <h3 class="text-3xl font-semibold">Question guide</h3>
           <div>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Non soluta
             neque voluptatem illo, blanditiis est, suscipit, fugit mollitia hic
