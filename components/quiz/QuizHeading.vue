195d5e403d2d4d0093fc458c6c2211096e621a3c
taychin5lab <tay.161.tod@gmail.com>
2020-12-08 02:10:25 +0700

    update function

---
+++ components/quiz/QuizHeading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class=" text-3xl" v-html="title"></h1>
+    <span v-if="error" class="text-red-600">{{ error }}</span>
+    <slot></slot>
+    <button class="mt-8" @click="$emit('next')">next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    error: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style></style>
--- pages/pick.vue
+++ pages/pick.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="container">
-   <!-- TODO: layout -->
+    <!-- TODO: layout -->
     <h1 class="text-3xl">Select the cards for yout feedback session</h1>
     <div class="grid grid-cols-6 gap-4">
       <div v-for="(card, i) in _roles" :key="`card-${i}`">
-        
-       <img @click="selectCard(card.slug)" :src="card.cardImage" alt="" class="shadow" :class="{'-active' : selectedCard.includes(card.slug) }">
+        <img
+          @click="selectCard(card.slug)"
+          :src="card.cardImage"
+          alt=""
+          class="shadow"
+          :class="{ '-active': selectedCard.includes(card.slug) }"
+        />
       </div>
-       <!-- TODO: IF NOT SELECTED DISABLE -->
+      <!-- TODO: IF NOT SELECTED DISABLE -->
       <div>
         <n-link :to="`/assign?roles=${_selectedRoleString}`">next</n-link>
       </div>
@@ -23,21 +28,24 @@ export default {
   }),
   computed: {
     _roles() {
+      // if(this.$route.query.roles) {
+      //   let selectedRoles = this.$route.query.roles.join(",")
+      //   return roles.filter(x => selectedRoles.includes(x.slug))
+      // }
       return roles;
     },
     _selectedRoleString() {
-      if(this.selectedCard.length) return this.selectedCard.join(",")
-      return []
+      if (this.selectedCard.length) return this.selectedCard.join(",");
+      return [];
     }
   },
   methods: {
-    selectCard(slug){
-      if(this.selectedCard.includes(slug)) {
-        this.selectedCard = this.selectedCard.filter(x => x !== slug)
+    selectCard(slug) {
+      if (this.selectedCard.includes(slug)) {
+        this.selectedCard = this.selectedCard.filter(x => x !== slug);
       } else {
-        this.selectedCard.push(slug)
+        this.selectedCard.push(slug);
       }
-      
     }
   }
 };
@@ -45,6 +53,6 @@ export default {
 
 <style lang="scss" scoped>
 .-active {
-  border: 2px solid plum
+  border: 2px solid plum;
 }
 </style>
