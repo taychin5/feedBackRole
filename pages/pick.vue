908bbc81205c9f878b21c82a707888376e84d3a7
tenpatrawin <w.patrawin@gmail.com>
2020-12-08 22:41:33 +0700

    ui fixed 02

--- pages/pick.vue
+++ pages/pick.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="container">
     <!-- TODO: layout -->
-    <h1 class="text-2xl text-center mb-10 font-bold">Select the cards for yout feedback session</h1>
+    <h1 class="text-2xl text-center mb-10 font-bold">
+      Select the cards for yout feedback session
+    </h1>
     <div class="grid grid-cols-6 gap-4">
       <div v-for="(card, i) in _roles" :key="`card-${i}`">
         <img
           @click="selectCard(card.slug)"
           :src="card.cardImage"
           alt=""
-          class="shadow cursor-pointer"
+          class="shadow cursor-pointer transform hover:scale-105 transition duration-400"
           :class="{ '-active': selectedCard.includes(card.slug) }"
         />
       </div>
       <!-- TODO: IF NOT SELECTED DISABLE -->
     </div>
-      <div class="flex justify-center mt-10 mb-20">
-        <n-link :to="`/assign?roles=${_selectedRoleString}`" class="px-6 py-2 rounded-full shadow bg-black text-white hover:bg-purple-600 hover:text-white transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">next</n-link>
-      </div>
+    <div class="flex justify-center mt-10 mb-20">
+      <n-link
+        :to="`/assign?roles=${_selectedRoleString}`"
+        class="px-6 py-2 rounded-full shadow bg-black text-white hover:bg-purple-600 hover:text-white transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+        >next</n-link
+      >
+    </div>
   </div>
 </template>
 
@@ -53,7 +59,7 @@ export default {
 
 <style lang="scss" scoped>
 .-active {
-  border: 4px solid #6A4DFF;
+  border: 4px solid #6a4dff;
   border-radius: 0.75rem;
 }
 </style>
