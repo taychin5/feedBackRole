40cf5245f839ffa7642dad07fcbb9b4c871f0649
tenpatrawin <w.patrawin@gmail.com>
2020-12-17 00:35:13 +0700

    Update 17 dec 20

---
+++ pages/howitswork.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>How its work</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped></style>
